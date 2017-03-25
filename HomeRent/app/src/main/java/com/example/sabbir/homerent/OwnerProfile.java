66edf35f3ded28fe90e661dc23a108aeac5b5882
DORBESH <mamun.incubator@gmail.com>
2017-03-25 23:37:39 +0600

    I am commitin now

---
+++ HomeRent/app/src/main/java/com/example/sabbir/homerent/OwnerProfile.java
@@ -0,0 +1,15 @@
+package com.example.sabbir.homerent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class OwnerProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_profile);
+        getSupportActionBar().hide();
+    }
+
+}
